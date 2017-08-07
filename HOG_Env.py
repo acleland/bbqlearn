0a96d2e54e3552ec9041684636f3b2cc848784a0
Andrew Cleland <alcleland@gmail.com>
2017-08-07 14:50:41 -0700

    added HOG_Env

---
+++ HOG_Env.py
@@ -0,0 +1,215 @@
+import numpy as np 
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+from collections import deque
+from scipy.special import expit
+from skimage import feature, color, exposure
+import sys
+
+from tools import *
+
+
+# Some constants
+
+IMAGE_PATH = "../Data/Train/"
+LABEL_PATH = "../Data/Skews_lowIOU/"
+OUTPUT_PATH = "../Output/"
+
+actions = ['left','right','up','down','bigger','smaller','fatter','taller','stop']
+num_actions = len(actions)
+history_length = 10
+ORIENTATIONS = 9
+PIXELS_PER_CELL = (16, 16)
+CELLS_PER_BLOCK = (3,3)
+BLOCK_NORM = 'L2'
+
+
+
+
+SHIFT = 5  # number of pixels box shifts by in each left, right, up, down action
+ZOOM_FRAC = 0.1  # Fraction box zooms by in bigger, smaller, taller, fatter actions
+
+PRINTING = False
+
+
+
+# HOG Stuff
+# --------------------------------------------------------------------------------
+def hog(img, visual):
+    resized = resize(img, (128,128))
+    gray = color.rgb2gray(np.array(resized))
+    return feature.hog(gray, orientations = ORIENTATIONS,
+                        pixels_per_cell = PIXELS_PER_CELL,
+                        cells_per_block = CELLS_PER_BLOCK,
+                        feature_vector = True,
+                        block_norm = BLOCK_NORM,
+                        visualise = visual)
+
+# --------------------------------------------------------------------------------
+
+# Sample picture just to compute feature length
+pdw1 = load_image('../Data/Train/pdw1.jpg')
+f = hog(pdw1, False)
+
+NUM_FEATURES = len(f)
+STATE_LENGTH = NUM_FEATURES + num_actions*history_length
+
+
+
+class State:
+    def __init__(self, image, box, history_length=history_length):
+        
+        self.image = image
+        self.box = box.copy()
+
+        self.actions = [self.left, self.right, self.up, self.down, self.bigger, self.smaller, self.fatter, self.taller, self.stop]
+        self.num_actions = len(self.actions)
+        
+        self.history_length = history_length
+        self.history = deque(maxlen=history_length)
+        for _ in range(history_length):
+            self.history.appendleft(np.zeros(self.num_actions))
+        self.readable_history = deque(maxlen=history_length)
+
+        self.shift = SHIFT
+        self.zoom_frac = ZOOM_FRAC
+        self.done = False
+
+    def __str__(self):
+        return self.image_name + '\n' + str(self.box)
+
+    def get_features(self,visual=False):
+        crop = get_crop(self.image, self.box)
+        return hog(crop, visual)
+
+    def get_vector(self):
+        features = self.get_features()
+        return np.concatenate((features, np.concatenate(self.history)))
+
+    def get_skew(self):
+        return self.image.crop(self.box.toVector2())
+
+    # Actions: (Don't call these explicitly, otherwise, state history  and features 
+    #           won't update correctly. Instead use state.take_action(action_number).)
+    def left(self):
+        self.box = self.box.adjust_x(-self.shift)
+    def right(self):
+        self.box = self.box.adjust_x(self.shift)
+    def up(self):
+        self.box = self.box.adjust_y(-self.shift) 
+    def down(self):
+        self.box = self.box.adjust_y(self.shift) 
+    def bigger(self):
+        self.box = self.box.zoom(1.0+self.zoom_frac)
+    def smaller(self):
+        self.box = self.box.zoom(1.0-self.zoom_frac)
+    def fatter(self):
+        self.box = self.box.adjust_width(1+self.zoom_frac)
+    def taller(self):
+        self.box = self.box.adjust_height(1+self.zoom_frac)
+    def stop(self):
+        self.done = True
+
+    def take_action(self, action_number):
+        self.actions[action_number]()
+        self.box = self.box.round()
+        self.history.appendleft(onehot(action_number, self.num_actions))
+        self.readable_history.appendleft(self.actions[action_number].__name__)
+
+# --------------------------------------------------------------------------------
+
+class HOG_Env:
+    def __init__(self):
+        self.actions = actions
+        self.num_actions = num_actions
+        self.num_features = NUM_FEATURES
+        self.state_length = STATE_LENGTH
+        self.state = None
+        self.episode = None    
+
+    def load(self, example):
+        imgf, bb, self.gt = parse_label(read_label(LABEL_PATH + example + '.labl'))
+        img = load_image(IMAGE_PATH + imgf + '.jpg')
+        self.state = State(img, bb, history_length)
+        self.episode = (self.gt, [bb])
+
+
+    def get_state(self):
+        return self.state.get_vector()
+
+    def take_action(self, a):
+        iou = self.state.box.iou(self.gt)
+        self.state.take_action(a)
+        self.episode[1].append(self.state.box)
+        new_iou = self.state.box.iou(self.gt)
+        r = np.sign(new_iou-iou)
+        return self.state.get_vector(), r
+
+    def show(self):
+        gt = self.episode[0]
+        hist = self.episode[1]
+        boxes = [gt]
+        boxes.append(hist[-1])
+        colors = ['y', 'r']
+        if len(hist) > 1:
+            boxes.append(hist[-2])
+            colors.append('w')
+        plot_img_boxes(self.state.image, boxes, colors)
+        show_hog(get_crop(self.state.image, self.state.box))
+        
+
+
+
+# --------------------------------------------------------------------------------
+
+def plot_img_boxes(img, boxes, colors=None):
+    fig,ax = plt.subplots(1)
+    ax.imshow(img)
+    # if colors is None:
+    #     colors = ['r']*len(boxes)
+   
+    for box, col in zip(boxes, colors):
+        rect = patches.Rectangle((box.x,box.y),box.width,box.height, linewidth=2,edgecolor=col,facecolor='none')
+        ax.add_patch(rect)
+    box_lefts = [box.x for box in boxes]
+    box_tops = [box.y for box in boxes]
+    box_rights = [box.x + box.width for box in boxes]
+    box_bottoms = [box.y + box.height for box in boxes]
+    margin = 100
+    img_box = img.getbbox()
+    left_xlim = max(0, min(box_lefts) - margin)
+    right_xlim = min(img_box[2], max(box_rights) + margin)
+    top_ylim = max(0, min(box_tops) - margin)
+    bottom_ylim = min(img_box[3], max(box_bottoms) + margin)
+    ax.set_xlim(left_xlim, right_xlim)
+    ax.set_ylim(bottom_ylim, top_ylim)
+    plt.show()
+
+def show_hog(image):
+    resized = resize(image, (128,128))
+    gray = color.rgb2gray(np.array(resized))
+    fd, hog_image = feature.hog(gray, orientations = ORIENTATIONS,
+                        pixels_per_cell = PIXELS_PER_CELL,
+                        cells_per_block = CELLS_PER_BLOCK,
+                        feature_vector = True,
+                        block_norm = BLOCK_NORM,
+                        visualise = True)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4), sharex=True, sharey=True)
+
+    ax1.axis('off')
+    ax1.imshow(gray, cmap=plt.cm.gray)
+    ax1.set_title('Input image')
+    ax1.set_adjustable('box-forced')
+
+    # Rescale histogram for better display
+    hog_image_rescaled = exposure.rescale_intensity(hog_image, in_range=(0, 0.02))
+    #hog_image_rescaled = hog_image
+
+    ax2.axis('off')
+    ax2.imshow(hog_image_rescaled, cmap=plt.cm.gray)
+    ax2.set_title('Histogram of Oriented Gradients')
+    ax1.set_adjustable('box-forced')
+    plt.show()
+    
