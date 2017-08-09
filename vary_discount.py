12e1cfa4e27b727ff09a0bbbf636df0ef293bb7f
Andrew Cleland <alcleland@gmail.com>
2017-08-09 07:55:38 -0700

    changed some parameters in vary_discount

--- vary_discount.py
+++ vary_discount.py
@@ -7,15 +7,15 @@ import pickle
 tlist = pickle.load(open('random_100.p','rb'))
 
 t = time.time()
-for disc_rate in [.1,.3,.5,.7,.9]:
+for disc_rate in [.1,.5,.9]:
     print('discount rate', disc_rate)
     print()
     qlearn = Qlearn(HOG_Env())
     qlearn.run('disc_'+str(disc_rate), tlist, 
-        num_epochs=2,
+        num_epochs=200,
         learning_rate=0.2,
         discount_factor=disc_rate, 
-        actions_per_episode=2,
+        actions_per_episode=15,
         visual = False)
 dt = time.time() - t
 print('time', dt)
